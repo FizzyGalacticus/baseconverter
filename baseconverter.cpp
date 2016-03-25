e2f6398131b306758e7717117d8939dccda54a5f
Dustin L. Dodson <dustinleedodson@gmail.com>
2016-03-25 11:53:03 -0800

    Added base converter file.

---
+++ baseconverter.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+#include <string>
+using std::string;
+using std::stoi;
+#include <algorithm>
+using std::reverse;
+
+int main(int argc, char const *argv[])
+{
+	char base_chars [] = {'0', '1', '2', '3',
+						  '4', '5', '6', '7',
+						  '8', '9', 'A', 'B',
+						  'C', 'D', 'E', 'F',
+						  'G', 'H', 'I', 'J',
+						  'K', 'L', 'M', 'N',
+						  'O', 'P', 'Q', 'R',
+						  'S', 'T', 'U', 'V',
+						  'W', 'X', 'Y', 'Z'};
+
+	if (argc > 2) {
+		auto m = stoi(argv[1]);
+		auto b = stoi(argv[2]);
+		auto n = -1;
+		string newBase = "";
+
+		while(m > 0) {
+			n+=1;
+			newBase += base_chars[m % b];
+			m = m / b;
+		}
+
+		reverse(newBase.begin(), newBase.end());
+		cout << newBase << endl;
+	}
+	else {
+		cout << "Usage: " << argv[0] << " <integer> <base>" << endl;
+	}
+
+	return 0;
+}
